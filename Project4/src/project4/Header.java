dd583eb452ce7e00c69acd788d7f893f32b0d81b
Matt Daumas <matthewdaumas@wireless-10-145-32-180.public.utexas.edu>
2016-07-18 18:52:53 -0500

    test

---
+++ Project4/src/project4/Header.java
@@ -0,0 +1,12 @@
+/* CRITTERS <MyClass.java>
+ * EE422C Project 4 submission by
+ * Replace <...> with your actual data.
+ * <Student1 Name>
+ * <Student1 EID>
+ * 76595
+ * <Student2 Name>
+ * <Student2 EID>
+ * 76595
+ * Slip days used: <0>
+ * Summer 2016
+ */
