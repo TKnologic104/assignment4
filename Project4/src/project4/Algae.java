48cee44f3d23d899cfab6b527119107a98e17be6
Matt Daumas <matthewdaumas@wireless-10-145-32-180.public.utexas.edu>
2016-07-18 19:11:21 -0500

    beep

--- Project4/src/project4/Algae.java
+++ Project4/src/project4/Algae.java
@@ -10,6 +10,6 @@ public class Algae extends TestCritter {
 	
 	public void doTimeStep() {
 		setEnergy(getEnergy() + Params.photosynthesis_energy_amount);
-		//terrible shithub implementation
+		//terrible gitlithub implementation
 	}
 }
