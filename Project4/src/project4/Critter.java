1bd0a303815c57fa6b8d7075e80fcdaabca749c0
Matt Daumas <matthewdaumas@192.168.0.4>
2016-07-21 00:10:36 -0500

    added coordinate getters and setters from branch of life

--- Project4/src/project4/Critter.java
+++ Project4/src/project4/Critter.java
@@ -233,4 +233,19 @@ public abstract class Critter {
 	public static void setPopulation(List<Critter> population) {
 		Critter.population = population;
 	}
+	public int getX_coord() {
+		return x_coord;
+	}
+
+	public void setX_coord(int x_coord) {
+		this.x_coord = x_coord;
+	}
+
+	public int getY_coord() {
+		return y_coord;
+	}
+
+	public void setY_coord(int y_coord) {
+		this.y_coord = y_coord;
+	}
 }
