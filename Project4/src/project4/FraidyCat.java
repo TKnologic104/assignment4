643c1a5743432c65a38d0dbed540de78d0800769
Tarang Khandpur <TKnologic104@gmail.com>
2016-07-22 18:32:45 -0500

    added the header to each of the Critters

--- Project4/src/project4/FraidyCat.java
+++ Project4/src/project4/FraidyCat.java
@@ -1,3 +1,16 @@
+/* CRITTERS <MyClass.java>
+ * https://github.com/MattDaumas/assignment4.git
+ * EE422C Project 4 submission by
+ * Tarang Khandpur
+ * tk8435
+ * 76595
+ * Matthew Daumas
+ * md32789
+ * 76595
+ * Slip days used: <1>
+ * Summer 2016
+ */
+
 package project4;
 
 //import project4.Critter.TestCritter;
