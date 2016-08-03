f4057e90506c73657775135fb7f98fa34b03fc25
Tarang Khandpur <TKnologic104@gmail.com>
2016-08-02 19:28:29 -0500

    updated UI

--- Project4/src/project4/Main.java
+++ Project4/src/project4/Main.java
@@ -1,15 +1,3 @@
-/* CRITTERS <MyClass.java>
- * https://github.com/MattDaumas/assignment4.git
- * EE422C Project 4 submission by
- * Tarang Khandpur
- * tk8435
- * 76595
- * Matthew Daumas
- * md32789
- * 76595
- * Slip days used: <1>
- * Summer 2016
- */
 package project4;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -30,28 +18,24 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.ToolBar;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.ArcType;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-//import project4.Critter.TestCritter;
-
 public class Main extends Application {
 
-//	static int i = 10;
-//	static String input = "START";
-//	static String delim = "[ ]+";
-//	static String[] tokens = new String[5]; 
-//	static Scanner in = new Scanner(System.in);
-			 
+	double framesPerSec = 1;	 
 	public static void main(String[] args) {
         launch(args);
     }
@@ -61,32 +45,84 @@ public class Main extends Application {
     	primaryStage.setTitle("Drawing Operations Test");
     	Group root = new Group();
 
-		Button btnQuit = new Button("QUIT");
-		btnQuit.setLayoutX(10);
-		btnQuit.setLayoutY(100);
+//		Button btnQuit = new Button("QUIT");
+//		btnQuit.setLayoutX(5);
+//		btnQuit.setLayoutY(10);
 		Button btnPause = new Button("PAUSE");
-		btnPause.setLayoutX(10);
-		btnPause.setLayoutY(150);
+		btnPause.setLayoutX(5);
+		btnPause.setLayoutY(10);
 		Button btnPlay = new Button("PLAY");
-		btnPlay.setLayoutX(10);
-		btnPlay.setLayoutY(200);
-    	RadioButton rb1 = new RadioButton("1/2");
-		RadioButton rb2 = new RadioButton("1/2");
-		RadioButton rb3 = new RadioButton("1/2");
+		btnPlay.setLayoutX(5);
+		btnPlay.setLayoutY(40);
+		ComboBox<String> critterCombo = new ComboBox();
+		critterCombo.getItems().addAll(
+				"Dumbo",
+				"Ent",
+				"FraidyCat",
+				"Tribble",
+				"ALL"
+		);
+		critterCombo.setLayoutX(5);
+		critterCombo.setLayoutY(70);
+		Label lb1 = new Label("Seed/Make Qty");
+		TextField num1 = new TextField();
+		lb1.setLayoutX(5);
+		lb1.setLayoutY(100);
+		num1.setLayoutX(5);
+		num1.setLayoutY(130);
+		num1.setMaxWidth(50);
+		Button btnSeed = new Button("SEED");
+		btnSeed.setLayoutX(5);
+		btnSeed.setLayoutY(160);
+		Button btnMake = new Button("MAKE");
+		btnMake.setLayoutX(5);
+		btnMake.setLayoutY(190);
+		Button btnStats = new Button("STATS");
+		btnStats.setLayoutX(5);
+		btnStats.setLayoutY(220);
+		
+    	RadioButton rb1 = new RadioButton("1/100");
+    	RadioButton rb2 = new RadioButton("1/50");
+    	RadioButton rb3 = new RadioButton("1/20");
+    	RadioButton rb4 = new RadioButton("1/10");
+    	RadioButton rb5 = new RadioButton("1/5");
+    	RadioButton rb6 = new RadioButton("1/2");
+		RadioButton rb7 = new RadioButton("1");
+		RadioButton rb8 = new RadioButton("2");
+		RadioButton rb9 = new RadioButton("5");
+		RadioButton rb10 = new RadioButton("10");
+		RadioButton rb11 = new RadioButton("20");
+		RadioButton rb12 = new RadioButton("50");
+		RadioButton rb13 = new RadioButton("100");
 		ToggleGroup tg = new ToggleGroup();
-		tg.getToggles().addAll(rb1, rb2, rb3);
-    	ToolBar toolbar = new ToolBar(
-    			new Button("SEED"),
-    			new Button("MAKE"),
-    			new Button("STATS"),
-    			new Button("+SPEED"),
-    			new Button("-SPEED")
-    			);
-    	toolbar.getItems().addAll(rb1, rb2, rb3, btnQuit, btnPause, btnPlay);
-    	toolbar.setOrientation(Orientation.VERTICAL);
-    	BorderPane pane = new BorderPane();
-    	pane.setRight(toolbar);
-
+		tg.getToggles().addAll(rb1, rb2, rb3, rb4, rb5, rb6, rb7, rb8, rb9, rb10, rb11, rb12, rb13);
+		rb1.setLayoutX(5);
+		rb1.setLayoutY(300);
+		rb2.setLayoutX(5);
+		rb2.setLayoutY(320);
+		rb3.setLayoutX(5);
+		rb3.setLayoutY(340);
+		rb4.setLayoutX(5);
+		rb4.setLayoutY(360);
+		rb5.setLayoutX(5);
+		rb5.setLayoutY(380);
+		rb6.setLayoutX(5);
+		rb6.setLayoutY(400);
+		rb7.setLayoutX(5);
+		rb7.setLayoutY(420);
+		rb8.setLayoutX(5);
+		rb8.setLayoutY(440);
+		rb9.setLayoutX(5);
+		rb9.setLayoutY(460);
+		rb10.setLayoutX(5);
+		rb10.setLayoutY(480);
+		rb11.setLayoutX(5);
+		rb11.setLayoutY(500);
+		rb12.setLayoutX(5);
+		rb12.setLayoutY(520);
+		rb13.setLayoutX(5);
+		rb13.setLayoutY(540);
+		
     	Timeline timeline = new Timeline();
 
 //    	root.getChildren().add(pane);
@@ -105,8 +141,87 @@ public class Main extends Application {
       			timeline.play();
       		}
       	});
+     	btnSeed.setOnAction(new EventHandler<ActionEvent>(){
+      		@Override
+      		public void handle(ActionEvent e){
+  				String input = num1.getText();
+  				int j = getNumber(input);
+  				if (j > 0){
+  					Critter.setSeed(j);
+  				}
+      		}
+      	});
+     	btnMake.setOnAction(new EventHandler<ActionEvent>(){
+      		@Override
+      		public void handle(ActionEvent e){
+      			System.out.println("make");
+      			try {
+      				String input = num1.getText();
+      				int j = getNumber(input);
+      				String str = critterCombo.getValue();
+      				str = "project4." + str;
+      				for (int i = 0; i < j; i++){
+      					Critter.makeCritter(str);
+      				}
+				} catch (InvalidCritterException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+      		}
+      	});
+     	btnStats.setOnAction(new EventHandler<ActionEvent>(){
+      		@Override
+      		public void handle(ActionEvent e){
+      			System.out.println("Stats");
+  				String input = num1.getText();
+  				String str = critterCombo.getValue();
+  				if (str.equals("ALL")){
+  					str = "project4.Critter";
+  				}
+  				else {
+  					str = "project4." + str;
+  				}
+  				try {
+					Critter.runStats(Critter.getInstances(str));
+				} catch (InvalidCritterException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+      		}
+      	});
+     	rb1.setOnAction(new EventHandler<ActionEvent>(){
+      		@Override
+      		public void handle(ActionEvent e){
+      			framesPerSec =  1.0/100.0;
+      		}
+        });
+    	rb13.setOnAction(new EventHandler<ActionEvent>(){
+      		@Override
+      		public void handle(ActionEvent e){
+      			framesPerSec =  100.0;
+      		}
+        });
         root.getChildren().add(btnPause);
         root.getChildren().add(btnPlay);
+        root.getChildren().add(critterCombo);
+        root.getChildren().add(lb1);
+        root.getChildren().add(num1);
+        root.getChildren().add(btnSeed);
+        root.getChildren().add(btnMake);
+        root.getChildren().add(btnStats);
+        root.getChildren().add(rb1);
+        root.getChildren().add(rb2);
+        root.getChildren().add(rb3);
+        root.getChildren().add(rb4);
+        root.getChildren().add(rb5);
+        root.getChildren().add(rb6);
+        root.getChildren().add(rb7);
+        root.getChildren().add(rb8);
+        root.getChildren().add(rb9);
+        root.getChildren().add(rb10);
+        root.getChildren().add(rb11);
+        root.getChildren().add(rb12);
+        root.getChildren().add(rb13);
         root.getChildren().add(canvas);
     	primaryStage.setScene(new Scene(root));
     	GraphicsContext gc = canvas.getGraphicsContext2D();
@@ -114,13 +229,19 @@ public class Main extends Application {
     	
         timeline.setCycleCount(Timeline.INDEFINITE); 
         KeyFrame kf = new KeyFrame(
-                 Duration.seconds(1),                // 60 FPS
+        		 Duration.seconds(framesPerSec),                // 60 FPS
                  new EventHandler<ActionEvent>(){
                      public void handle(ActionEvent ae)
                  {
                  // Clear the canvas
                  gc.clearRect(Params.world_width * 1, 0, Params.world_width * 5, Params.world_height * 5);
-                 Critter.worldTimeStep();
+                 int j = 1;
+                 if (framesPerSec > 1.0){
+                	 j = (int) (framesPerSec);
+                 }
+                 for (int i = 0; i < j; i++){
+                     Critter.worldTimeStep();
+                 }
                  drawShapes(gc);
                  }
              }
@@ -135,14 +256,6 @@ public class Main extends Application {
     }
 
     private void drawShapes(GraphicsContext gc) {
-		/*
-		    	try {
-					Critter.makeCritter("project4.Dumbo");
-				} catch (InvalidCritterException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-		*/
 		gc.setFill(Color.GREEN);
 		gc.setFont(new Font(STYLESHEET_CASPIAN,10));
 		for (int i = 1; i <= Params.world_height;i++){
@@ -153,171 +266,16 @@ public class Main extends Application {
 //						Image img = new Image(getClass().getResourceAsStream("Dumbo.jpg"));
 //						Image img = new Image("Beetle.png");
 //						gc.drawImage(img, 10 + j * 5, i * 5, 5, 5);
-//						gc.fillText(str, 10 + j * 5, i * 5);
-						gc.fillRect(Params.world_width + j * 5, i * 5, 5, 5);
+						gc.fillText(str, Params.world_width + j * 5, i * 5);
+//						gc.fillRect(Params.world_width + j * 5, i * 5, 5, 5);
 						break;
 					}
 				}
 			}
 		}
 	}
-}
-    
-    
-//		Critter.setSeed(3);
-//		
-//		Craig c = new Craig();
-//		c.setEnergy(Params.start_energy);
-//		c.setXCoord(5); 
-//		c.setYCoord(5);
-//		Critter.TestCritter.setPopulation(c);
-//		
-//		FraidyCat f = new FraidyCat();
-//		f.setEnergy(Params.start_energy);
-//		f.setXCoord(5); 
-//		f.setYCoord(5);
-//		Critter.TestCritter.setPopulation(f);
-//		
-//		Tribble t0 = new Tribble();
-//		t0.setEnergy(Params.start_energy);
-//		t0.setXCoord(5); 
-//		t0.setYCoord(5);
-//		Critter.TestCritter.setPopulation(t0);
-//		
-//		Tribble t = new Tribble();
-//		t.setEnergy(Params.start_energy);
-//		t.setXCoord(3); 
-//		t.setYCoord(5);
-//		Critter.TestCritter.setPopulation(t);
-//		
-//		for (Critter a: Critter.TestCritter.getPopulation()) {
-//			for (Critter b: Critter.TestCritter.getPopulation()) {
-//				TestCritter a1 = (TestCritter) a;
-//				TestCritter b1 = (TestCritter) b;
-//				if (a1 != b1 && a1.getXCoord() == b1.getXCoord() && a1.getYCoord() == b1.getYCoord()) {
-//					Critter.TestCritter.resolveEncounter(a1,b1);
-//				}
-//			}
-//		}
-//		
-//		Critter.TestCritter.resolveEncounter(c, f);
-//		Critter.TestCritter.cullDead();
-		
-/*
-		while (!(input.toUpperCase().equals("QUIT"))) {
-			System.out.print("Critters>");
-			input = in.nextLine();
-			tokens = input.trim().split(delim);
-			switch (tokens[0]){
-			case "quit":
-				if (tokens.length > 1){
-					System.out.println("Invalid Command: " + input);
-					break;
-				}
-				input = tokens[0];
-				break;
-			case "show":
-				if (tokens.length > 1){
-					System.out.println("Invalid Command: " + input);
-					break;
-				}
-				Critter.displayWorld();
-				break;
-			case "step":
-				if (tokens.length > 2){
-					System.out.println("Invalid Command: " + input);
-					break;
-				}
-				i = 1;
-				if (tokens.length > 1){
-					i = getNumber(tokens[1]);
-				}
-				for (int j = 1 ; j <= i; j++){
-					Critter.worldTimeStep();
-				}
-				break;
-			case "seed":
-				if (tokens.length > 2){
-					System.out.println("Invalid Command: " + input);
-					break;
-				}
-				i = 1;
-				if (tokens.length > 1){
-					i = getNumber(tokens[1]);
-				}
-				i = getNumber(tokens[1]);
-				Critter.setSeed(i);
-				break;
-			case "make":
-				if (tokens.length > 3){
-					System.out.println("Invalid Command: " + input);
-					break;
-				}
-				i = 1;
-				if (tokens.length > 2){
-					i = getNumber(tokens[2]);
-				}
-				for (int j = 1 ; j <= i; j++){
-					Critter.makeCritter(tokens[1]);
-				}
-				break;
-			case "stats":
-				if (tokens.length > 2){
-					System.out.println("Invalid Command: " + input);
-					break;
-				}
-				if (!(tokens.length == 2)){
-					System.out.println("Invalid Command: " + input);
-					break;
-				}
-				List<Critter> statList = new ArrayList<Critter>();
-				statList = Critter.getInstances(tokens[1]);
-				try{
-					if (statList.size() <= 0){
-						System.out.println("No instances found");
-						break;
-					}
-				}
-				catch (NullPointerException e){
-					break;
-				}
-				if (statList.equals(Critter.TestCritter.getPopulation())){
-					Critter.runStats(statList);
-					break;
-				}
-				Object ob = null;
-				Method m1 = null; // super class to all methods of any particular class
-				Class<?> cls = Class.forName(tokens[1]);
-				ob = cls.newInstance();
-				try {
-					m1 = ob.getClass().getMethod("runStats", List.class);
-				} catch (NoSuchMethodException | SecurityException e) {
-					System.out.println("Invalid Command: " + input);
-					return;
-					//e.printStackTrace();
-				}
-				try {
-					m1.invoke(ob, statList);
-				} catch (IllegalArgumentException | InvocationTargetException e) {
-					System.out.println("Invalid Command: " + input);
-					return;
-					//e.printStackTrace();
-				}
-				break;
-			default:
-				//if (!(input.equals("START"))){
-					System.out.println("Invalid Command: " + input);
-			//}
-				 break;
-			}
-		}
-		System.out.println("Bye");	
-	/** converts string into an integer value, and also prints Invalid command if its not a valid integer.
-	 * @param str
-	 * @return
-	 */
-/*
-	public static int getNumber(String str){
+
+    public static int getNumber(String str){
 		int i = 0;
 		if (str == null || str == "" || str == " "){
 			return 1;
@@ -326,14 +284,14 @@ public class Main extends Application {
 			i = Integer.parseInt(str);
 		}
 		 catch(NumberFormatException er) {
-			 System.out.println("Invalid Command: " + input);
+			 System.out.println("Invalid Number:");
 			 return 0;
 		}
 		if (i < 1){
-			 System.out.println("Invalid Command:" + input);
+			 System.out.println("Invalid Number:");
 			 return 0;
 		}
 		return i;
 	}
-*/
+}
 
